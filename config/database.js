68b3b3deafb4a9d91d6ba8ffd5e7f3f31de4fd4e
Isaac Farciert <rfisaac2296@yahoo.com>
2021-11-05 11:21:38 -0400

    add local development database information

--- config/database.js
+++ config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'kiosklocal'),
+        username: env('DATABASE_USERNAME', 'strapicms'),
+        password: env('DATABASE_PASSWORD', ''),
+        schema: env('DATABASE_SCHEMA', 'public')        
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
